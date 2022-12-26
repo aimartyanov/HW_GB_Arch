40d8549283e375371d95b19e71640b32e8adf778
aimartyanov <aimartyanov@gmail.com>
2022-12-26 19:17:46 +0300

    arch

---
+++ Final Project/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArr(string[] array)
+{
+    Console.WriteLine("Ввод массива");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
+void PrintArr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+int NewArrLen(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+string[] FillNewArray(string[] array, string[] NewArray)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            NewArray[j] = array[i];
+            j++;
+        }
+    }
+    return NewArray;
+}
+string[] array = new string[5];
+FillArr(array);
+Console.WriteLine();
+PrintArr(array);
+string[] NewArray = new string[NewArrLen(array)];
+FillNewArray(array, NewArray);
+PrintArr(NewArray);
