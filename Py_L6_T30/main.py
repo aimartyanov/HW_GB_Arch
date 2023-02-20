48936141bdffde5af3844c8d046d8af395c2def8
aimartyanov <aimartyanov@gmail.com>
2023-02-20 09:38:19 +0300

    arch

---
+++ Py_L6_T30/main.py
@@ -0,0 +1,5 @@
+a1 = int(input())
+d = int(input())
+n = int(input())
+for i in range(n):
+    print(a1 + i * d)
---
+++ Py_L6_T32/main.py
@@ -0,0 +1,6 @@
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
